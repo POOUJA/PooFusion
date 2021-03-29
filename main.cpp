2635371a598fc9534a78445d469fca18914afecb
Ángel Luis <algarcia@ujaen.es>
2021-03-29 17:57:51 +0200

    Commit inicial

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author algarcia
+ *
+ * @date 29 de marzo de 2021, 17:57
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ *
+ */
+int main ( int argc, char** argv )
+{
+
+   return 0;
+}
+
